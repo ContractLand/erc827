219a0ddb87f4c6ccadccd2bfd9ec7b8f6ba14874
Yuming He <yuming.p.he@gmail.com>
2018-09-04 21:47:17 -0400

    Init ERC827 Token

---
+++ contracts/mocks/ERC827TokenMock.sol
@@ -0,0 +1,13 @@
+pragma solidity 0.4.24;
+
+import "../ERC827Token.sol";
+
+// mock class using ERC827 Token
+contract ERC827TokenMock is ERC827Token {
+
+  constructor(address initialAccount, uint256 initialBalance) public {
+    balances[initialAccount] = initialBalance;
+    totalSupply_ = initialBalance;
+  }
+
+}
